ab25b3b4ce1860955efccdb7c0c3f1731989ae14
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-11-22 20:57:42 +0000

    Update docs after building Travis build 11 of charlottenosam/kmos_tools
    The docs were built from the branch 'master' against the commit
    da0868b4813818eab9349fde47233c6b346bd772.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/charlottenosam/kmos_tools/jobs/615749961.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.post7+gfb011ff',
+    VERSION: '0.1.0.post8+gda0868b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
