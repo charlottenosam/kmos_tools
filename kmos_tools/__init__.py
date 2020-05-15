e54c72bf3487a2cc793407db2f1c1a81ffd011b0
Charlotte Mason <charlotte.nosam@gmail.com>
2020-05-15 10:12:02 +0100

    added import io which was missing from __init__.py

--- kmos_tools/__init__.py
+++ kmos_tools/__init__.py
@@ -3,9 +3,10 @@ from __future__ import division
 from __future__ import print_function
 
 from .exposure import *
+from .io import *
+from .pipeline_fixes import *
 from .sky_clean import *
 from .star_offsets import *
-from .pipeline_fixes import *
 from .tools import *
 
 from ._version import get_versions
